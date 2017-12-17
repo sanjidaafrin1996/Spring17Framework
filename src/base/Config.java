e8436d130bdd0108e7018dcdcb6e6e358618eaba
DESKTOP-2A6JV2R\Hasan <taltekc@gmail.com>
2017-12-17 13:23:22 -0500

    dataProvider and xls reader

--- src/base/Config.java
+++ src/base/Config.java
@@ -3,15 +3,16 @@ package base;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import utils.webDriverApi;
 
 public class Config extends webDriverApi {
 
-	
+
 	// this is the configuration class
-	
+
 	@BeforeMethod
 	public void setUp(){
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Hasan\\workspace\\Spring17Framework\\drivers\\chromedriver.exe");
@@ -19,8 +20,10 @@ public class Config extends webDriverApi {
 		driver.get("https://www.facebook.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+	}
 
-		
+	@AfterMethod
+	public void tearDown (){
+		//driver.quit();
 	}
 }
